26c76bd463bba047b4aded342697807023e16c40
JubairAronno <153675988+JubairAronno@users.noreply.github.com>
2023-12-25 19:09:14 +0600

    Update README.c
    Set the student data, Update count of boys and girls,Add the student to the beginning of the linked list,

--- README.c
+++ README.c
@@ -9,10 +9,8 @@ float cgpa;
 char gender;
 struct student* next;
 };
-
 int boycount=0;
 int girlcount=0;
-
 void addstudent(struct student**head,int rollnumber,const char*name,float cgpa,char gender)
 {
     for(struct student*current=*head;current!=NULL;current=current->next)
@@ -31,6 +29,36 @@ void addstudent(struct student**head,int rollnumber,const char*name,float cgpa,c
         printf("Memory allocation failed.\n");
         exit(1);
     }
+}
+
+newStudent->rollNumber=rollNumber;
+    strncpy(newStudent->name,name,sizeof(newStudent->name)-1);
+    newStudent->name[sizeof(newStudent->name)-1]='\0';
+    newStudent->cgpa=cgpa;
+    newStudent->gender=gender;
+    if(gender=='MALE'||gender=='male') 
+        {
+        boysCount++;
+    }else if(gender=='FEMALE'||gender=='female') {
+        girlsCount++;
+    }
+    newStudent->nex =*head;
+    *head=newStudent;
+
+    printf("Student added.\n");
+}
+void findStudentByRollNumber(struct Student*head,int rollNumber) {
+    while(head!=NULL)
+        {
+        if(head->rollNumber==rollNumber) 
+        {
+            printf("\nStudent found:\n");
+            printf("Roll Number: %d, Name: %s,CGPA: %.2f,Gender: %c\n",head->rollNumber,head->name,head->cgpa,head->gender);
+            return;
+        }
+    head = head->next;
+    }
+    printf("Student with roll number %d not found.\n", rollNumber);
 }
 
 
